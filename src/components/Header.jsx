bfddbb2227eec4f118b24636e5f9b71d660bf847
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-03 20:08:11 +0330

    complete top header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { BsTelephoneFill } from "react-icons/bs"
+import { MdEmail } from "react-icons/md"
+import "../styles/header.css";
 
 function Header() {
     return (
-        <div>
+        <header className='header'>
+            <div className='header_top'>
+                <div className='header_question'>
+                    <div>
+                        <span className='header_question_text'>آیا سوالی دارید؟</span>
+                    </div>
+                    <div className='header_question_group'>
+                        <span><BsTelephoneFill /></span>
+                        <span>
+                            <a className='header_a_tag' href="#">
+                                ۲۲٦۲۲۱۱۹
+                            </a>
+                        </span>
+                    </div>
+                    <div className='header_question_group'>
+                        <span><MdEmail /></span>
+                        <span>
+                            <a className='header_a_tag' href="#">
+                                edu@shatel.ir
+                            </a>
+                        </span>
+                    </div>
+                </div>
+                <div className='header_auth_links'>
+                    <div>
+                        <a className='header_a_tag' href="#">
+                            ثبت نام
+                        </a>
+                    </div>
+                    <span>|</span>
+                    <div>
+                        <a className='header_a_tag' href="#">
+                            ورود
+                        </a>
+                    </div>
+                </div>
+            </div>
             <div>Header</div>
             <div>Header</div>
-            <div>Header</div>
-        </div>
+        </header>
     )
 }
 
